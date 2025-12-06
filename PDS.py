e79f8f94d87477e4bf818921b2d74c48a465299e
20本ネギ <s.kazuki0111@gmail.com>
2025-12-06 23:56:11 +0900

    1階層中までみれるようにした

--- PDS.py
+++ PDS.py
@@ -1,11 +1,23 @@
 import os
 import pathlib
+from pathlib import Path
+
+_cd=""
+def _display_directory(_list):
+    for i in _list:
+        print("├──"+i)
+        _cd=_now_directory+"\\"+i
+        if not Path(_cd).is_file():
+            print("│    ├──",os.listdir(Path(_cd)))
+
 
 _now_directory=input("現在の絶対パスを入力してください")
+
 #C:\Users\skazu\Documents\python #test用
 _directory_list=os.listdir(_now_directory)
 #print(os.listdir(_now_directory))
+f=Path(_now_directory)
+print(f.is_file())
 
 print(_now_directory)
-for i in _directory_list:
-    print("├──"+i)
+_display_directory(_directory_list)
