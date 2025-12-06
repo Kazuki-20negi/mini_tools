cb19533b5c430ab28b37b98ac1e8dd9af61fd3e9
20本ネギ <s.kazuki0111@gmail.com>
2025-12-06 22:27:16 +0900

    Update PDS.py

--- PDS.py
+++ PDS.py
@@ -2,5 +2,10 @@ import os
 import pathlib
 
 _now_directory=input("現在の絶対パスを入力してください")
+#C:\Users\skazu\Documents\python #test用
+_directory_list=os.listdir(_now_directory)
+#print(os.listdir(_now_directory))
 
-print(os.listdir(_now_directory))
+print(_now_directory)
+for i in _directory_list:
+    print("├──"+i)
