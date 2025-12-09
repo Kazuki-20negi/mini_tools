269576ea7bdb1dd071a61fb3df841875cf32867a
20本ネギ <s.kazuki0111@gmail.com>
2025-12-10 02:48:21 +0900

    枝の改善と色付け

--- PDS.py
+++ PDS.py
@@ -1,22 +1,36 @@
 import os
 import pathlib
 from pathlib import Path
+RED = '\033[31m'
+BLUE = '\033[34m'
+GREEN = '\033[32m'
+RESET = '\033[0m'
 
-
-def _display_directory(_cd, _list,_level):
+def _display_directory(_cd, _list,_prefix):
     for i in _list:
         target_path = _cd / i
-        #if _list.index(i)==len(_list):
-        #    print("│ "*_level+"└──"+i)
-        print("│ "*_level+"├─"+i)
-        if not Path(target_path).is_file():
-            _display_directory(target_path, os.listdir(target_path),_level+1)
+        if _list.index(i)==len(_list)-1:
+            if not Path(target_path).is_file():
+                print(_prefix+"└─"+GREEN+i+RESET)
+                _display_directory(target_path, os.listdir(target_path),_prefix+"  ")
+            else:
+                print(_prefix+"└─"+i)
+
+        else:
+            if not Path(target_path).is_file():
+                print(_prefix+"├─"+GREEN+i+RESET)
+                _display_directory(target_path, os.listdir(target_path),_prefix+"│ ")
+            else:
+                print(_prefix+"├─"+i)
+        #if not Path(target_path).is_file():
+        #    _display_directory(target_path, os.listdir(target_path),_prefix)
 
 _now_directory=input("現在の絶対パスを入力してください")
 
 #C:\Users\skazu\Documents\python #test用
-_directory_list=os.listdir(_now_directory)
+
 _now_directory=Path(_now_directory)
+_directory_list=os.listdir(_now_directory)
 print(_now_directory)
 _cd=_now_directory
-_display_directory(_now_directory,_directory_list,0)
+_display_directory(_now_directory,_directory_list,"")
