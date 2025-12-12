8862f6b3ef52c288becf9713f805ae2f9f912af1
20本ネギ <s.kazuki0111@gmail.com>
2025-12-12 21:24:22 +0900

    参照除外機能のついか

--- PDS.py
+++ PDS.py
@@ -9,31 +9,33 @@ RESET = '\033[0m'
 def _display_directory(_cd, _list,_prefix, a):
     for i in _list:
         target_path = _cd / i
-        if i==a:
-            pass
-        elif _list.index(i)==len(_list)-1:
-            if not Path(target_path).is_file():
-                print(_prefix+"└─"+GREEN+i+RESET)
+        if not Path(target_path).is_file():
+            i2=GREEN+i+RESET
+        else:i2=i
+
+        if _list.index(i)==len(_list)-1:
+            if not Path(target_path).is_file() and not i in a:
+                print(_prefix+"└─"+i2)
                 _display_directory(target_path, os.listdir(target_path),_prefix+"  ",a)
             else:
-                print(_prefix+"└─"+i)
+                print(_prefix+"└─"+i2)
 
         else:
-            if not Path(target_path).is_file():
-                print(_prefix+"├─"+GREEN+i+RESET)
+            if not Path(target_path).is_file() and not i in a:
+                print(_prefix+"├─"+i2)
                 _display_directory(target_path, os.listdir(target_path),_prefix+"│ ",a)
             else:
-                print(_prefix+"├─"+i)
+                print(_prefix+"├─"+i2)
         #if not Path(target_path).is_file():
         #    _display_directory(target_path, os.listdir(target_path),_prefix)
 
 
 def setting():
     global a
-    a=input("除外するパス:")
+    a=input("中身を参照しないフォルダ名(,区切りスペースなし):").split(",")
 
 
-_now_directory=input("現在の絶対パスを入力してください")
+_now_directory=input("絶対パスを入力,または'setting'で設定に移行")
 if _now_directory=="setting":
     setting()
 _now_directory=input("現在の絶対パスを入力してください")
