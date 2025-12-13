64fa8f2314f3eb53c3263d354ef8ac882d1a3940
20本ネギ <s.kazuki0111@gmail.com>
2025-12-13 22:22:19 +0900

    Update PDS.py

--- PDS.py
+++ PDS.py
@@ -31,18 +31,18 @@ def _display_directory(_cd, _list,_prefix, _except_list):
         if index == len(_list) - 1:
             if not Path(target_path).is_file() and not i in _except_list:
                 print(_prefix+"└─"+i2)
-                _display_directory(target_path, os.listdir(target_path),_prefix+"  ",_except_list)
+                _display_directory(target_path, sorted(os.listdir(target_path)),_prefix+"  ",_except_list)
             else:
                 print(_prefix+"└─"+i2)
 
         else:
             if not Path(target_path).is_file() and not i in _except_list:
                 print(_prefix+"├─"+i2)
-                _display_directory(target_path, os.listdir(target_path),_prefix+"│ ",_except_list)
+                _display_directory(target_path, sorted(os.listdir(target_path)),_prefix+"│ ",_except_list)
             else:
                 print(_prefix+"├─"+i2)
         #if not Path(target_path).is_file():
-        #    _display_directory(target_path, os.listdir(target_path),_prefix)
+        #    _display_directory(target_path, sorted(os.listdir(target_path)),_prefix)
 
 
 def setting():
@@ -53,13 +53,13 @@ def setting():
 _now_directory=input("絶対パスを入力,または'setting'で設定に移行")
 if _now_directory=="setting":
     setting()
-_now_directory=input("現在の絶対パスを入力してください")
+_now_directory=input("絶対パスを入力してください")
 
 #C:\Users\skazu\Documents\python #test用
 
 
 _now_directory=Path(_now_directory)
-_directory_list=os.listdir(_now_directory)
+_directory_list=sorted(os.listdir(_now_directory))
 print(_now_directory)
 _cd=_now_directory
 _display_directory(_now_directory,_directory_list,"",_except_list)
