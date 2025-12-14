1165d2493c120c1e1c5101b915912063daa8df58
20本ネギ <s.kazuki0111@gmail.com>
2025-12-14 18:07:21 +0900

    Update PDS.py

--- PDS.py
+++ PDS.py
@@ -9,7 +9,7 @@ RESET = '\033[0m'
 _except_list=[]
 _output_lines=""
 
-def _file_size(_path):
+def _file_size(_path): #ファイルサイズを単位付きで返す
     _size=float(os.path.getsize(_path))
     if os.path.getsize(_path)>=1024:
         _size=_size/1024
@@ -24,6 +24,7 @@ def _file_size(_path):
         return " ("+f"{_size:.1f}B)"
 
 def _display_directory(_cd, _list,_prefix, _except_list):
+    #ディレクトリツリーを表示
     global _output_lines
     for index, i in enumerate(_list):
         target_path = _cd / i
@@ -48,20 +49,17 @@ def _display_directory(_cd, _list,_prefix, _except_list):
             else:
                 print(_prefix+"├─"+i2)
                 _output_lines+=(_prefix+"├─"+i+"\n")
-        #if not Path(target_path).is_file():
-        #    _display_directory(target_path, sorted(os.listdir(target_path)),_prefix)
 
-
-def setting():
+def setting(): #特定フォルダの除外
     global _except_list
     _except_list=input("中身を参照しないフォルダ名(,区切りスペースなし):").split(",")
 
-def copy_clipboard(_output_lines):
+def copy_clipboard(_output_lines): #クリップボードへのコピー
     subprocess.run("clip", input=_output_lines, text=True, encoding='cp932') 
     print("クリップボードにコピーしました！")
 
 
-while True:
+while True: # ユーザ入力の受付とエラーチェック
     _now_directory=input("絶対パスを入力,または'setting'で設定に移行")
     if _now_directory=="setting":
         setting()
@@ -74,15 +72,15 @@ while True:
         if not Path(_now_directory).is_dir():
             print(f"{RED}エラー: ファイルが指定されています。フォルダのパスを入力してください。{RESET}")
             continue
-    #
+
     try:
-        _temp_list=sorted(os.listdir(_now_directory))
+        _temp_list_a=sorted(os.listdir(_now_directory))
         break
     except PermissionError:
         print(f"{RED}エラー: アクセス権限がありません。別の場所を指定してください。{RESET}")
         continue
     except Exception as e:
-        print(f"{RED}予期せぬエラーが発生しました: {e}{RESET}")
+        print(f"{RED}エラー: {e}{RESET}")
         continue
 
 _output_lines+=(_now_directory+"\n")
@@ -91,5 +89,5 @@ _directory_list=sorted(os.listdir(_now_directory))
 print(_now_directory)
 _cd=_now_directory
 _display_directory(_now_directory,_directory_list,"",_except_list)
-if input("クリップードにコピーしますか？ (y/n):")=="y":
+if input("クリップードにコピーしますか？ (y/n):").lower()=="y":
     copy_clipboard(_output_lines)
