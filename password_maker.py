6fdc07e7eb38be4c3255f7c70c660155b6911bfd
20本ネギ <s.kazuki0111@gmail.com>
2025-12-16 00:34:52 +0900

    Update password_maker.py

--- password_maker.py
+++ password_maker.py
@@ -1,19 +1,23 @@
 import secrets
 import argparse
 import string
-parser = argparse.ArgumentParser(description='password maker')    # 2. パーサを作る
+
+#パーサ作成
+parser = argparse.ArgumentParser(description='password maker')
 parser.add_argument("arg1",help="桁数")
 args = parser.parse_args()
 
-#a=secrets.token_urlsafe(int(args.arg1))
-#print(a[0:int(args.arg1)])
+#文字種定義
 alphabet = string.ascii_letters + string.digits
 punctuation = "-*/._:;[]#()"
+
+#パスワード生成
 while True:
     password="".join(secrets.choice(alphabet+punctuation)for i in range(int(args.arg1)))
     if (any(c.islower() for c in password)
             and any(c.isupper() for c in password)
             and sum(c.isdigit() for c in password)>=2
-            and any(not(c.isalnum()) for c in password)):
+            and any(c in punctuation for c in password)):
         break
+
 print(password)
