1e57316bf371f05eebed0ed81ab337c95b06037b
20本ネギ <s.kazuki0111@gmail.com>
2025-12-21 01:26:16 +0900

    Update stalin_sort.py

--- stalin_sort.py
+++ stalin_sort.py
@@ -1,8 +1,7 @@
 #sample: [1, 3, 1, 5, 8, 2, 6, 4, 9, 6, 1, 4]
 input_list=list(map(int,input().split(",")))
 
-result=[]
-result.append(input_list[0])
+result=[input_list[0]]
 for i in range(1,len(input_list)-1):
     if input_list[i-1]<=input_list[i] and result[-1]<=input_list[i]:
         result.append(input_list[i])
