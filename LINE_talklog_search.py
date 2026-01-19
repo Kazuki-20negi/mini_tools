e6f1ba7835bb4a2a86097d97acfd54d9293331c2
20本ネギ <s.kazuki0111@gmail.com>
2026-01-19 21:41:03 +0900

    Update LINE_talklog_search.py

--- LINE_talklog_search.py
+++ LINE_talklog_search.py
@@ -1,10 +1,8 @@
 import re
 
 def search_in_file(file_path, pattern):
-    with open(file_path, "r", encoding="utf-8") as f:
-        content= f.read()
-    matches= re.findall(pattern, content, re.MULTILINE)
-    for match in matches:
-        print(f"Found: '{match.group()}' at position {match.start()}-{match.end()}")
-
+    with open(file_path, 'r', encoding='utf-8') as f:
+            for i, line in enumerate(f, 1):
+                if re.search(pattern, line):
+                    print(f"{i}行目: {line.strip()}")
 search_in_file("test.txt", r"が参加しました")
