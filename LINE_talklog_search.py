53e8be6cc1933ade980c91ba089d3744cc434907
20本ネギ <s.kazuki0111@gmail.com>
2026-01-20 21:11:36 +0900

    Update LINE_talklog_search.py

--- LINE_talklog_search.py
+++ LINE_talklog_search.py
@@ -3,13 +3,16 @@ from collections import defaultdict
 
 def search_in_file(file_path, pattern):
     data=[]
+    match_count=0
     with open(file_path, 'r', encoding='utf-8') as f:
             for i, line in enumerate(f, 1):
                 if re.search(r"^\d{4}/\d{2}/\d{2}",line):
                      data.append(line.strip())
                 if re.search(pattern, line):
-                    print(f"{i}行目: {data[-1]}{line.strip()}")
-file_name="test.txt"
+                    #print(f"{i}行目: {data[-1]}{line.strip()}")
+                    match_count+=1
+    return match_count
+
 
 def count_user_speaking(file_path, pattern):
     char_counts = defaultdict(int)
@@ -39,10 +42,13 @@ def count_user_speaking(file_path, pattern):
                         char_counts[current_user] += len(clean_msg)
                         
     return char_counts
+file_name="hogehoge.txt"
 
-print("---参加ログ---")
-search_in_file(file_name, r"が参加しました")
-print("\n---退会ログ---")
-search_in_file(file_name, r"が退会しました")
+#print("---参加ログ---")
+#search_in_file(file_name, r"が参加しました")
+#print("\n---退会ログ---")
+#search_in_file(file_name, r"が退会しました")
+print(f"累計参加者：{search_in_file(file_name, r"が参加しました")}人")
+
 print("\n---発言字数---")
-print(dict(count_user_speaking(file_name,r"さてらいと")))
+print(dict(count_user_speaking(file_name,r"20本ネギ")))
