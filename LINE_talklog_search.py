0b889cfc6c2293a63e92f07b49d7eca5ecde802d
20本ネギ <s.kazuki0111@gmail.com>
2026-01-20 20:29:49 +0900

    Update LINE_talklog_search.py

--- LINE_talklog_search.py
+++ LINE_talklog_search.py
@@ -1,8 +1,15 @@
 import re
 
 def search_in_file(file_path, pattern):
+    data=[]
     with open(file_path, 'r', encoding='utf-8') as f:
             for i, line in enumerate(f, 1):
+                if re.search(r"^\d{4}/\d{2}/\d{2}",line):
+                     data.append(line.strip())
                 if re.search(pattern, line):
-                    print(f"{i}行目: {line.strip()}")
-search_in_file("test.txt", r"が参加しました")
+                    print(f"{i}行目: {data[-1]}{line.strip()}")
+file_name="test.txt"
+
+search_in_file(file_name, r"が参加しました")
+
+search_in_file(file_name, r"が退会しました")
