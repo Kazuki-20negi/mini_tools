c30175d7d25757b4e1453a52877cc65144ef0e23
20本ネギ <s.kazuki0111@gmail.com>
2026-01-25 00:55:17 +0900

    Update num_in_text.py

--- num_in_text.py
+++ num_in_text.py
@@ -2,7 +2,9 @@ import re
 import sys
 
 while True:
+    print("入力してください：")
     text=sys.stdin.read()
     num=re.findall(r"\d+",text)
     total=sum(int(n) for n in num)
     print(f"合計：{total}")
+    print("-"*20)
