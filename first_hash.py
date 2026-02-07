e9b97442acea6981156d3336166ce38050b7f3aa
20本ネギ <s.kazuki0111@gmail.com>
2026-02-08 00:02:48 +0900

    Update first_hash.py

--- first_hash.py
+++ first_hash.py
@@ -3,7 +3,8 @@ import hashlib
 text="はじめてのハッシュ"
 byte_text=text.encode("utf-8")
 
-hash_object=hashlib.sha256(byte_text)
+hash_object=hashlib.sha512(byte_text)
 hex_dig=hash_object.hexdigest()
 
 print(hex_dig)
+print(f"len:{len(hex_dig)}")
