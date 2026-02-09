71b499dc344e40071bca8a9cda79d4432199133e
20本ネギ <s.kazuki0111@gmail.com>
2026-02-09 23:29:45 +0900

    Update first_hash.py

--- first_hash.py
+++ first_hash.py
@@ -8,3 +8,11 @@ hex_dig=hash_object.hexdigest()
 
 print(hex_dig)
 print(f"len:{len(hex_dig)}")
+
+# 同一文字列からは同一のハッシュ
+hash_object2=hashlib.sha512(byte_text)
+hex_dig2=hash_object2.hexdigest()
+
+print("\n同一文字列をハッシュ化")
+print(hex_dig2)
+print(f"len:{len(hex_dig2)}")
