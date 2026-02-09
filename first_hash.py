5fc810b1db760a4a2b99d66bdd248ebf29879f87
20本ネギ <s.kazuki0111@gmail.com>
2026-02-09 23:31:59 +0900

    Update first_hash.py

--- first_hash.py
+++ first_hash.py
@@ -16,3 +16,16 @@ hex_dig2=hash_object2.hexdigest()
 print("\n同一文字列をハッシュ化")
 print(hex_dig2)
 print(f"len:{len(hex_dig2)}")
+
+# ハッシュによる改ざん判定
+text_base="改ざん検知のテスト1"
+text_same="改ざん検知のテスト1"
+text_diff="改ざん検知のテスト2"
+
+def calculate_hash(text):
+    """文字列を受け取り、SHA-256ハッシュ値を返す関数"""
+    # バイト列に変換
+    byte_data = text.encode('utf-8')
+    # ハッシュ化
+    hex_digest = hashlib.sha256(byte_data).hexdigest()
+    return hex_digest
