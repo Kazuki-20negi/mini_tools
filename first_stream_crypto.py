b5c24bf9fd56c7412f3981eb466119ec84bf91cf
20本ネギ <s.kazuki0111@gmail.com>
2026-02-20 23:45:49 +0900

    Update first_stream_crypto.py

--- first_stream_crypto.py
+++ first_stream_crypto.py
@@ -6,5 +6,7 @@ def stream(text,key):
     key=ord(key)
     text=ord(text)
     key=key*(len(text)//len(key))+key[:len(text)%len(key)]
+    result=""
     for i in range(len(text)):
-        
+        result+=text[i]^key[i]
+    return result
