20d3954e59978766abc57d391b55588ca23b618f
20本ネギ <s.kazuki0111@gmail.com>
2026-02-21 00:00:26 +0900

    Update first_stream_crypto.py

--- first_stream_crypto.py
+++ first_stream_crypto.py
@@ -2,11 +2,14 @@
 key="abc"
 text="helloworld"
 
-def stream(text,key):
-    key=ord(key)
-    text=ord(text)
+def stream_encrypt(text,key):
     key=key*(len(text)//len(key))+key[:len(text)%len(key)]
     result=""
     for i in range(len(text)):
-        result+=text[i]^key[i]
+        xor_val=ord(text[i])^ord(key[i])
+        result+=f"{xor_val:02x}"
+        #result+=chr(xor_val)
     return result
+
+encrypted_text = stream(text, key)
+print(encrypted_text)
